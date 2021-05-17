710cfbb390bdcc69204bbd9f8904412a7726435e
vladimirbaranetsky <dcvgfr5@gmail.com>
2021-05-17 15:25:24 +0300

    updated component login”

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,33 @@ const Login = () => {
                     <span className="content__header-title">LOGIN</span>
                 </div>
                 <div className="content__body">
+                    <div className="h2"><h2>Welcome back</h2>
+                    </div>
+                    <div className="login__container">
 
+                        <form>
+                            <div className="login__row">
+
+                                <div className="login__col">
+                                    <input type="text" name="email" placeholder="name@gmail.com"/>
+                                    <input type="password" name="password" placeholder="Enter your password..."/>
+                                    <span className="psw"><a href="#">Forgot password?</a></span>
+                                    <input type="submit" value="Login"/>
+                                </div>
+
+                                <div className="vl">
+                                    <span className="inner">or</span>
+                                </div>
+
+                                <div className="login__col">
+                                    <a href="#" className="fb btn">Continue with Facebook</a>
+                                    <a href="#" className="apple btn">Continue with Apple</a>
+                                    <a href="#" className="google btn">Continue with Google</a>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
