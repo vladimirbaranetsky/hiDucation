701b7a9cc2edf4bf6f3a2fddf7f479898cf3feb3
vladimirbaranetsky <dcvgfr5@gmail.com>
2021-05-29 08:42:08 +0300

    added registration

--- src/components/shell/body/registration/Registration.jsx
+++ src/components/shell/body/registration/Registration.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import style from "./Registration.module.css";
+import {AiOutlineUser} from "react-icons/ai";
+import {RiLockPasswordLine} from "react-icons/ri";
 
 const Registration = () => {
     return <React.Fragment>
@@ -11,6 +13,50 @@ const Registration = () => {
                 <span className={style.registration__header_title}>Registration</span>
             </div>
         </div>
+
+        <div className={style.registration__wrapper}>
+            <form>
+                <div className={style.registration__row}>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>First Name</span>
+                        <input className={style.registration__item_input} type="text" name="firstName"
+                               placeholder='Enter your first name...'/>
+                    </div>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>Last Name</span>
+                        <input className={style.registration__item_input} type="text" name="lastName"
+                               placeholder='Enter your last name...'/>
+                    </div>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>Date of Birth</span>
+                        <input className={style.registration__item_input} type="date" name="dateOfBirth"/>
+                    </div>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>Email</span>
+                        <input className={style.registration__item_input} type="email" name="email"
+                               placeholder="name@email.com"/>
+                        <AiOutlineUser className={style.registration__item_input_icon}/>
+                    </div>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>Password</span>
+                        <input className={style.registration__item_input} type="password" name="password"
+                               placeholder="Enter your password..."/>
+                        <RiLockPasswordLine className={style.registration__item_input_icon}/>
+                        <a>Use 8 or more characters with a mix of letters and numbers</a>
+                    </div>
+                    <br/>
+                    <div className={style.registration__item}>
+                        <span className={style.registration__item_title}>Confirm your password</span>
+                        <input className={style.registration__item_input} type="password" name="confirmPassword"/>
+                    </div>
+                    <div className={style.registration__item}>
+                        <button className={style.registration__item_button} type="submit" value="signUp">Sign up</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
     </React.Fragment>
 }
 
