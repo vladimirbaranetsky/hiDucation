00c30588e8fb6b4fc4abfbba8fe8e462070f0299
Dmitrii Kosach <dkosach79@gmail.com>
2021-06-08 16:58:18 +0300

    updated regExp in config

--- src/config/config-credentials.js
+++ src/config/config-credentials.js
@@ -1,3 +1,3 @@
 export const DIGITS_NAME = 4;
 export const DIGITS_PASSWORD = 8;
-export const VALIDATE = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+export const VALIDATE = RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
