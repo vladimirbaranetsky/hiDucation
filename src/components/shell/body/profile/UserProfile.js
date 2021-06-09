3e96a4c00ae73a1ecab1dd4ff1c38946f87c16ad
vladimirbaranetsky <dcvgfr5@gmail.com>
2021-06-09 19:18:30 +0300

    update rootReducer

--- src/components/shell/body/profile/UserProfile.js
+++ src/components/shell/body/profile/UserProfile.js
@@ -14,6 +14,34 @@ const Profile = () => {
                 </div>
                 <span className={style.profile__header_title}>Profile</span>
             </div>
+            {/*<div className={style.profile__body}>
+                <form>
+                    <div className={style.profile__row}>
+                        <div className={style.profile__col}>
+                            <div className={style.profile__item}>
+                                <label className={style.profile__item_title}>First Name</label>
+                                <input className={style.profile__item_input} name="firstName" value=""/>
+                            </div>
+                            <div className={style.profile__item}>
+                                <label className={style.profile__item_title}>Last Name</label>
+                                <input className={style.profile__item_input} name="lastName" value=""/>
+                            </div>
+                            <div className={style.profile__item}>
+                                <label className={style.profile__item_title}>Institute</label>
+                                <input className={style.profile__item_input} name="institute" value=""/>
+                            </div>
+                            <div className={style.profile__item}>
+                                <label className={style.profile__item_title}>Degree</label>
+                                <input className={style.profile__item_input} name="degree" value=""/>
+                            </div>
+                            <div className={style.profile__item}>
+                                <label className={style.profile__item_title}>Email</label>
+                                <input className={style.profile__item_input} name="email" value=""/>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>*/}
         </div>
     </React.Fragment>
 }
--- src/reducers/reducer-root.js
+++ src/reducers/reducer-root.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 
 import {authentication} from "./reducer-auth";
 import {send} from "./reducer-message";
+import {profileUpdate} from "./reducer-profile";
 
 export default combineReducers({
     data: authentication,
-    message: send
+    message: send,
+    profileUpdate: profileUpdate
 });
 
